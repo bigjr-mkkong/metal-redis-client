644269f8100eaeda5e65cb51b74ccde8d44aac42
Boming Kong <michaelkongboming@gmail.com>
2024-08-09 16:30:22 -0700

    Implement basic read/write to Named pipe
    So now it is able to receive PONG after sending out PING

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,56 @@
 extern crate redis;
 
+
 use redis::cmd;
+use std::fs::File;
+use std::path::Path;
+use std::ffi::OsStr;
+use std::io::prelude::*;
+
+fn open_pipe(_pipe_path: &str, readable: bool, writable: bool) -> std::io::Result<File> {
+    let pipe_path = Path::new(_pipe_path);
+
+    let mut fo = File::options().read(readable).write(writable).open(pipe_path);
+
+    return fo;
+}
+
 
 fn main() -> redis::RedisResult<()> {
     let client = redis::Client::open("redis://127.0.0.1")?;
-    
     let mut con = client.get_connection()?;
 
     let ping_cmd = cmd("PING");
 
-    let resp:String = ping_cmd.query(&mut con)?;
-
-    println!("{}", resp);
+    // let resp:String = ping_cmd.query(&mut con)?;
+    // println!("{}", resp);
 
     let packed_cmd = ping_cmd.get_packed_command();
 
-    println!("{}", String::from_utf8(packed_cmd).unwrap());
+
+    println!("{}", String::from_utf8(packed_cmd.clone()).unwrap());
     
+
+    match open_pipe("/tmp/redis-pipe0", false, true) {
+        Err(why) => {
+            panic!("Failed to open pipe: {}", why);
+        }
+        Ok(mut pipe0) => {
+            pipe0.write_all(&packed_cmd)?;
+        }
+    };
+
+    match open_pipe("/tmp/redis-pipe1", true, false) {
+        Err(why) => {
+            panic!("Failed to open pipe: {}", why);
+        }
+        Ok(mut pipe1) => {
+            let mut retcmd = String::new();
+            pipe1.read_to_string(&mut retcmd)?;
+
+            println!("readed response: {}", retcmd);
+        }
+    };
+
     Ok(())
 }
